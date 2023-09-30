62102b638ec945920a5cbb8086372b119fa57428
Nathan Holmes <ndholmes@Nathans-MBP.hosts.dmsilva.com>
2023-09-30 18:20:23 -0400

    handimation

--- GodPoolGame/TestSubjects/TestBall.gd
+++ GodPoolGame/TestSubjects/TestBall.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
-
+var stored_force = Vector2.ZERO
 onready var pull_back_component = $PullBackComponent
 onready var pointer_sprite = $Pointer
 onready var pull_back_trail_container = $PullBackTrail
@@ -16,11 +16,10 @@ func _process(delta):
 	rotation = 0
 
 func _on_PullBackComponent_pull_back_released(force):
-	apply_impulse(Vector2.ZERO, force)
+	stored_force = force
 	pointer_sprite.visible = false
 	pointer_sprite.rotation = 0
-	pull_back_trail_container.visible = false
-	pull_back_trail_container.rotation = 0
+
 
 func _on_PullBackComponent_pulling_back(direction, power_ratio):
 	set_pull_back_trail(direction,power_ratio)
@@ -42,7 +41,7 @@ func set_pull_back_trail(direction, power_ratio):
 	var length: float = Vector2.ZERO.distance_to(direction)
 	var angle = Vector2.ZERO.angle_to_point(direction) - 1.5708
 	pull_back_trail_container.rotation = angle
-	pull_back_trail_container.position = - direction * .5
+	pull_back_trail_container.position = - direction * .2
 	pull_back_trail_container.visible = true
 
 func set_pointer(direction, power_ratio):
@@ -54,3 +53,10 @@ func set_pointer(direction, power_ratio):
 	pointer_sprite.rotation = angle
 	pointer_sprite.position = direction * .5
 	pointer_sprite.visible = true
+
+func _on_FlickTimmer_timeout():
+	apply_impulse(Vector2.ZERO, stored_force)
+	stored_force = Vector2.ZERO
+	pull_back_trail_container.visible = false
+	pull_back_trail_container.rotation = 0
+
