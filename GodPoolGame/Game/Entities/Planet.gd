a50209aab318e7cf6f10ca3a29d2de9aba6c7ada
Dave Silva <github@dmsilva.com>
2023-10-01 19:24:51 -0400

    Add planet and hole art

--- GodPoolGame/Game/Entities/Planet.gd
+++ GodPoolGame/Game/Entities/Planet.gd
@@ -1,10 +1,52 @@
 extends CelestialBody
 class_name Planet
 
-export (int) var population = 0
+export (int) var population = 0 setget _set_population
+
+func _ready():
+	randomize()
+	var bases = [
+		preload("res://Assets/Planets/Base/plain.png"),
+		preload("res://Assets/Planets/Base/lumpy.png"),
+		preload("res://Assets/Planets/Base/spike.png"),
+		preload("res://Assets/Planets/Base/disco.png"),
+		preload("res://Assets/Planets/Base/super.png"),
+	]
+	var effects = [
+		preload("res://Assets/Planets/Effects/bubble.png"),
+		preload("res://Assets/Planets/Effects/clouds.png"),
+		preload("res://Assets/Planets/Effects/goopy.png"),
+		preload("res://Assets/Planets/Effects/ring-moon.png"),
+		preload("res://Assets/Planets/Effects/squiggles.png"),
+		preload("res://Assets/Planets/Effects/vine.png"),
+	]
+	var faces = [
+		preload("res://Assets/Planets/Faces/grin.png"),
+		preload("res://Assets/Planets/Faces/hyper.png"),
+		preload("res://Assets/Planets/Faces/mellow.png"),
+		preload("res://Assets/Planets/Faces/oh.png"),
+		preload("res://Assets/Planets/Faces/stinker.png"),
+	]
+	$Sprite.texture = bases[randi() % 5]
+	$Sprite/Effect.texture = effects[randi() % 5]
+	$Sprite/Face.texture = faces[randi() % 5]
+	
+	if population == 0:
+		$Sprite/Face.visible = false
 
 func has_life() -> bool:
 	return population > 0
+
+
+func _set_population(p):
+	population = p
+	if $Sprite/Face == null:
+		return
+	
+	if population == 0:
+		$Sprite/Face.visible = false
+	else:
+		$Sprite/Face.visible = true
 
 
 func _add_to_groups():
