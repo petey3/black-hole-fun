f42d6a885eff63883d9cd6d6a06e4792cb0dd911
Dave Silva <github@dmsilva.com>
2023-10-02 10:59:39 -0400

    Reset music on level change and restart

--- GodPoolGame/Game/MusicMan.gd
+++ GodPoolGame/Game/MusicMan.gd
@@ -5,12 +5,14 @@ var times_shrunk: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	EventServices.dispatch().subscribe(EnterNextUniverseStateEvent.ID, self, "_on_universe_change_event")
-	$DroneBass.unmute()
+	EventServices.dispatch().subscribe(NextLevelEvent.ID, self, "_on_next_level_event")
+	EventServices.dispatch().subscribe(ReloadedCurrentSceneEvent.ID, self, "_on_reload_current_scene_event")
+	$Chordz.unmute()
 
 func update_music_state():
 	match(times_shrunk):
 		0:
-			$DroneBass.fade_in()
+			$DroneBass.fade_in(0.5)
 		1:
 			$Chordz.fade_in()
 		2:
@@ -22,4 +24,18 @@ func update_music_state():
 
 func _on_universe_change_event(event: Event):
 	times_shrunk = times_shrunk + 1
+	update_music_state()
+
+func _on_next_level_event(event: Event):
+	self.times_shrunk = 0
+	$Chordz.fade_out()
+	$TwinkleSynth.fade_out()
+	$TenseBass.fade_out(0.5)
+	update_music_state()
+
+func _on_reload_current_scene_event(event: Event):
+	self.times_shrunk = 0
+	$Chordz.fade_out()
+	$TwinkleSynth.fade_out()
+	$TenseBass.fade_out(0.5)
 	update_music_state()
