7423cf6a45beba3f295c0ce9a7ed93b42a4ed6a3
petey3 <gitpetey3@gmail.com>
2023-10-02 16:02:51 -0400

    Improve level entity registration

--- GodPoolGame/Game/Levels/Level.gd
+++ GodPoolGame/Game/Levels/Level.gd
@@ -2,7 +2,9 @@ extends Node2D
 class_name Level
 
 var level_entity_info = null
+var has_run_first_frame = false
 
-func _ready():
-	yield(self, "ready")
-	GameplayServices.entities().register_current_level(self)
+func _process(delta):
+	if not has_run_first_frame:
+		GameplayServices.entities().register_current_level(self)
+		has_run_first_frame = true
