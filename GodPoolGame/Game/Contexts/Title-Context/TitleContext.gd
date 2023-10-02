ce1226f13cf7999beed99abdbed17f83105cf4e8
Nathan Holmes <ndholmes@Nathans-MacBook-Pro.local>
2023-10-02 00:19:11 -0400

    add jens main screen

--- GodPoolGame/Game/Contexts/Title-Context/TitleContext.gd
+++ GodPoolGame/Game/Contexts/Title-Context/TitleContext.gd
@@ -4,19 +4,19 @@ class_name TitleContext
 const CONTEXT_ID = "context.title"
 
 onready var color_rect = $ColorRect
+onready var start_button = $ColorRect/StartButton
 
 func context_id_string() -> String:
 	return CONTEXT_ID
 
 
 func _ready():
+	start_button.connect("pressed", self, "_on_start_clicked")
 	PresentationServices.ui_service.set_root_control(color_rect)
 	
-	
-func _process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		_on_go_to_game_pressed()
-	
+func _on_start_clicked():
+	_on_go_to_game_pressed()
 	
 func _on_go_to_game_pressed():
+	print('hello')
 	change_context(GameplayContext.CONTEXT_ID)
